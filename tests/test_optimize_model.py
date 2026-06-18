f973e875aad5fbf29b7166972eff1b80fa0a5554
DennisLarsson <dennis.j.larsson@gmail.com>
2026-06-18 13:31:20 +0200

    Try with seed not self

--- tests/test_optimize_model.py
+++ tests/test_optimize_model.py
@@ -17,7 +17,7 @@ class TestOptimizeModel(unittest.TestCase):
         self.assertIn('RandomForestRegressor', search_spaces)
 
     def test_create_stacking_regressor(self):
-        self.seed = 42
+        seed = 42
         # Mock models
         mock_models = [
             ('lr', LinearRegression()),
